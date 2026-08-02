ff1d5f8a57373224706212e6906c0abd1dc2d23f
Deepak Mehta <98341180+DeepakMehta8054@users.noreply.github.com>
2026-08-02 14:16:58 +0530

    Update Map.js

--- Map.js
+++ Map.js
@@ -430,6 +430,67 @@ marker.bindPopup(`
 
     if (latlngs.length > 0) {
         map.fitBounds(movementLine.getBounds());
+      updateStatistics(locations);
     }
+
+}
+
+function updateStatistics(locations) {
+
+    // Total Records
+    document.getElementById("totalRecords").textContent =
+        excelData.length;
+
+    // Displayed Records
+    document.getElementById("displayedRecords").textContent =
+        locations.length;
+
+    // Unique Towers
+    let towers = new Set();
+
+    locations.forEach(loc => {
+
+        if (loc.tower)
+            towers.add(loc.tower);
+
+    });
+
+    document.getElementById("uniqueTowers").textContent =
+        towers.size;
+
+    // Distance
+    let totalDistance = 0;
+
+    for (let i = 1; i < locations.length; i++) {
+
+        totalDistance += calculateDistance(
+            locations[i - 1].lat,
+            locations[i - 1].lng,
+            locations[i].lat,
+            locations[i].lng
+        );
+
+    }
+
+    document.getElementById("totalDistance").textContent =
+        totalDistance.toFixed(2);
+
+}
+function calculateDistance(lat1, lon1, lat2, lon2) {
+
+    const R = 6371;
+
+    const dLat = (lat2 - lat1) * Math.PI / 180;
+    const dLon = (lon2 - lon1) * Math.PI / 180;
+
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(lat1 * Math.PI / 180) *
+        Math.cos(lat2 * Math.PI / 180) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return R * c;
 
 }
